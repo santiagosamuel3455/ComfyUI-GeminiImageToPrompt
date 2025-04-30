64bd57a3b0d9f3322903f55965a417d41d5148ed
santiagosamuel3455 <santiagosamuel3455@gmail.com>
2025-04-30 04:20:16 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,15 +3,17 @@
 # Importar los nodos personalizados
 from .gemini_prompt_node import GeminiImageToPromptNode
 from .gemini_text_to_prompt_node import GeminiTextToCinematicPromptNode
+from .deepseek_r1_klingai_node import DeepseekR1KlingAINode
 
 # Mapeo interno de clases de nodos para ComfyUI
 NODE_CLASS_MAPPINGS = {
     "GeminiImageToPromptNode": GeminiImageToPromptNode,
-    "GeminiTextToCinematicPromptNode": GeminiTextToCinematicPromptNode
+    "GeminiTextToCinematicPromptNode": GeminiTextToCinematicPromptNode,
+    "DeepseekR1KlingAINode": DeepseekR1KlingAINode
 }
 
 # Nombres que mostrará la UI de ComfyUI para estos nodos
 NODE_DISPLAY_NAME_MAPPINGS = {
     "GeminiImageToPromptNode": "Gemini Image to Prompt",
-    "GeminiTextToCinematicPromptNode": "Gemini Text to Cinematic Prompt"
-}
+    "GeminiTextToCinematicPromptNode": "Gemini Text to Cinematic Prompt",
+    "DeepseekR1KlingAINode": "Deepseek R1 KlingAI Text/Image to Video Node"
